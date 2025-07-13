d4aeed0ac9ad0cdaab97171172fee2c92378907a
dwi-enercloud <dwi.putro@enercloud.io>
2025-07-14 00:47:10 +0700

    feat: update build.gradle for React Native configuration, remove MainApplication.java, and add document picker dependency

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,11 +1,18 @@
 import Constants from 'expo-constants';
 
 export const generateAPIUrl = (relativePath: string) => {
-  const origin = Constants.experienceUrl.replace('exp://', 'http://');
-
   const path = relativePath.startsWith('/') ? relativePath : `/${relativePath}`;
 
   if (process.env.NODE_ENV === 'development') {
+    // Handle different development environments
+    let origin = 'http://localhost:8081';
+
+    if (Constants.experienceUrl) {
+      origin = Constants.experienceUrl.replace('exp://', 'http://');
+    } else if (Constants.debuggerHost) {
+      origin = `http://${Constants.debuggerHost.split(':')[0]}:8081`;
+    }
+
     return origin.concat(path);
   }
 
